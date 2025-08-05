53c4ad1fb748f64ca6385cc7e16c982e656bf642
naratorn.b <naratorn.b@kasikornline.com>
2025-08-05 13:52:53 +1000

    update to allocated eip

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.27.213.199:5001', // live
+  baseURL: 'http://54.66.213.198:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
